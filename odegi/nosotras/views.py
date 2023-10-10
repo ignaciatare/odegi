aa779ef40a3edb3da313c206d4556c80bb4bee13
Ignacia Taré Albornoz <101875879+ignaciatare@users.noreply.github.com>
2023-10-10 13:05:33 -0300

    funcionando: todo lo anterior más nosotras

--- odegi/nosotras/views.py
+++ odegi/nosotras/views.py
@@ -3,8 +3,8 @@ from .models import Nosotras
 
 
 def equipoodegi(request):
-    equipo = Nosotras.objects.filter(voluntaria=False).order_by('-nombre')
-    voluntarias = Nosotras.objects.filter(voluntaria=True).order_by('-nombre')
+    equipo = Nosotras.objects.filter(voluntaria=False)
+    voluntarias = Nosotras.objects.filter(voluntaria=True)
     context = {
         'equipo': equipo,
         'voluntarias': voluntarias,
--- odegi/odegiblog/urls.py
+++ odegi/odegiblog/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('buscar/', views.buscar_publicaciones, name='buscar_publicaciones'),
     path('categoria/<str:categoria_nombre>/', views.posts_por_categoria, name='posts_por_categoria'),
     path('publicacion/<slug:slug>/', views.publicacion, name='ver_publicacion'),
-    path('transparencia', views.transparencia)
+    path('transparencia', views.transparencia, name='transparencia')
 ]
